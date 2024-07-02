1c4965a6ef41d952223cf94d7c9ca807dd321392
VeronicaWayne <veronica.palyne@gmail.com>
2024-07-02 07:10:43 +0300

    1

---
+++ Assets/scripts/CharachterController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharachterController : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    [SerializeField] private GameObject target;
+
+    void Start()
+    {
+      
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = target.transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
